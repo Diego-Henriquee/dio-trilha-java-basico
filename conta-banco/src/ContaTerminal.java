787ed91b7853382c9d9dbe9af61d3bf3535a5955
Diego Henrique <diegohenrique.g12@gmail.com>
2025-07-25 01:11:27 -0300

    finalizando projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrance = new Scanner (System.in);
+
+        System.out.println("Qual é o seu nome?");
+        String name = entrance.nextLine();
+        
+        System.out.println("Olá, " + name +"! Sou o Atendente Virtual do DIOBANK. Vamos abrir sua conta.");
+
+        System.out.println("Qual é a sua renda mensal");
+        double income = entrance.nextDouble();
+
+        int account = 1021;
+        String agency = "067-8";
+    
+        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + account + " e seu saldo " + income + " já está disponível para saque.");
     }
 }
